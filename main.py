8c7d99825488eb98bdbfa9f49c9acc82bc1a6ce8
CalvinMiller190 <67594438+CalvinMiller190@users.noreply.github.com>
2020-08-28 18:41:39 -0400

    Changed the body
    Changed the edited body to use f strings. Much cleaner and easier.

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ while True:
     ratio = submission.upvote_ratio
     upvotes = round((ratio*submission.score)/(2*ratio-1)) if ratio != 0.5 else round(submission.score/2)
     downvotes = upvotes-submission.score
-    edited = str(upvotes) + ' upvotes,' + '\n\n' + str(downvotes) + ' downvotes,' + '\n\n' + 'and ' + str(submission.num_comments) + ' comments!' + '\n\n' + 'Credit to u/Krukerfluk and Tom Scott!' + '\n\n' + 'Code: https://github.com/CalvinMiller190/reddit-post' + '\n\n' + 'This post uses the [praw module](https://praw.readthedocs.io/en/latest/) to get the information about the post, then edits the post every second based on the information that it got!'
+    edited = f"{str(upvotes)} upvotes,\n\n{str(downvotes)} downvotes,\n\nand {str(submission.num_comments)} comments!\n\nCredit to u/Krukerfluk and Tom Scott!\n\nCode: https://github.com/CalvinMiller190/reddit-post\n\nThis post uses the [praw module](https://praw.readthedocs.io/en/latest/) to get the information about the post, then edits the post every second based on the information that it got!"
     submission.edit(edited)
     changes += 1
     print(f"Changed {changes} times!")
