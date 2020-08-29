fe3d1c8b2837a5c3c383b9e92d385945665eea3d
CalvinMiller190 <67594438+CalvinMiller190@users.noreply.github.com>
2020-08-28 20:26:50 -0400

    Added edit count

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ while True:
     ratio = submission.upvote_ratio
     upvotes = round((ratio*submission.score)/(2*ratio-1)) if ratio != 0.5 else round(submission.score/2)
     downvotes = upvotes-submission.score
-    edited = f"{str(upvotes)} upvotes,\n\n{str(downvotes)} downvotes,\n\nand {str(submission.num_comments)} comments!\n\nCredit to u/Krukerfluk and Tom Scott!\n\nCode: https://github.com/CalvinMiller190/reddit-post\n\nThis post uses the [praw module](https://praw.readthedocs.io/en/latest/) to get the information about the post, then edits the post every second based on the information that it got!"
+    edited = f"{str(upvotes)} upvotes,\n\n{str(downvotes)} downvotes,\n\nand {str(submission.num_comments)} comments!\n\nEdits: {changes}\n\nCredit to u/Krukerfluk and Tom Scott!\n\nCode: https://github.com/CalvinMiller190/reddit-post\n\nThis post uses the [praw module](https://praw.readthedocs.io/en/latest/) to get the information about the post, then edits the post every second based on the information that it got!"
     submission.edit(edited)
     changes += 1
+    pickle.dump(changes, open("changes.db", "wb"))
     print(f"Changed {changes} times!")
