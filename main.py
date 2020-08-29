007ee9dc81f2e7dd82cd4d1c76b899226f87b242
HenryBass <63601449+HenryBass@users.noreply.github.com>
2020-08-29 19:02:17 -0400

    Added line breaks.
    Dividing line 17 into three lines can help with readability.

--- main.py
+++ main.py
@@ -6,15 +6,17 @@ reddit = praw.Reddit(client_id="****",
     password="****",
     user_agent="****")
 
-changes = 15500
+changes = 14000
 
 while True:
+    submission = reddit.submission(id="****")
     score = submission.score
-    awards = len(submission.all_awardings)
     ratio = submission.upvote_ratio
     upvotes = round((ratio*score)/(2*ratio-1)) if ratio != 0.5 else round(score/2)
     downvotes = upvotes-score
-    edited = f"{str(upvotes)} upvotes,\n\n{str(downvotes)} downvotes,\n\n{str(submission.num_comments)} comments, \n\n{score} score,\n\nand {awards} awards!\n\n\n\nEdits: {changes} since Friday, August 28, 2020 at 8:30PM EST.\n\nCredit to u/Krukerfluk and Tom Scott!\n\nCode: https://github.com/CalvinMiller190/reddit-post\n\nThis post uses the [praw module](https://praw.readthedocs.io/en/latest/) to get the information about the post, then edits the post every second based on the information that it got!"
+    edited = f"{str(upvotes)} upvotes,\n\n{str(downvotes)} downvotes,\n\nand {str(submission.num_comments)} comments!\n\nScore: {score}\n\nEdits: {changes} since Friday, August 28, 2020 at 8:30PM EST." \
+             f"\n\nCredit to u/Krukerfluk and Tom Scott!\n\nCode: https://github.com/CalvinMiller190/reddit-post\n" \
+             f"\nThis post uses the [praw module](https://praw.readthedocs.io/en/latest/) to get the information about the post, then edits the post every second based on the information that it got!"
     submission.edit(edited)
     changes += 1
     print(f"Changed {changes} times!")
